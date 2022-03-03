2a3a87b007e7f576ea7d3b6926278500fd9d64ee
mark-escosura <escosuramarkse@gmail.com>
2022-03-03 00:43:16 -0800

    Displaying each BudgetCard created to the dom by mapping over the budgets array from the useBudgets context returning each and every Budget cards, Amount is saying 'NaN' because we don't actually have a budget amount yet. Our amount is stored in our expenses

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { useState } from 'react';
 import { Button, Stack } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import AddBudgetModal from './components/AddBudgetModal';
 import BudgetCard from './components/BudgetCard';
+import { useBudgets } from './contexts/BudgetsContext';
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const { budgets } = useBudgets();
+
   return (
     <>
       <Container className='my-4'>
         <Stack direction='horizontal' gap='2' className='mb-4'>
           <h1 className='me-auto'>Budgets</h1>
-          <Button variant='primary'>Add Budget</Button>
+          <Button variant='primary' onClick={() => setShowAddBudgetModal(true)}>
+            Add Budget
+          </Button>
           <Button variant='outline-primary'>Add Expense</Button>
         </Stack>
         <div
@@ -20,14 +27,20 @@ function App() {
             alignItems: 'flex-start',
           }}
         >
-          <BudgetCard
-            name='Entertainment'
-            amount={1100}
-            max={1000}
-          ></BudgetCard>
+          {budgets.map((budget) => (
+            <BudgetCard
+              key={budget.id}
+              name={budget.name}
+              amount={budget.amount}
+              max={budget.max}
+            />
+          ))}
         </div>
       </Container>
-      <AddBudgetModal show />
+      <AddBudgetModal
+        show={showAddBudgetModal}
+        handleClose={setShowAddBudgetModal}
+      />
     </>
   );
 }
