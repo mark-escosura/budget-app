37e73461d1826a82cf1e961dd293a8809f9d695f
mark-escosura <escosuramarkse@gmail.com>
2022-03-05 03:50:05 -0800

    Expenses are all showing in the View Expense Modal and deleteButton is functioning

--- src/components/ViewExpenses.js
+++ src/components/ViewExpenses.js
@@ -3,10 +3,13 @@ import {
   UNCATEGORIZED_BUDGET_ID,
   useBudgets,
 } from '../contexts/BudgetsContext';
+import { currencyFormatter } from '../utils';
 
 export default function ViewExpensesModal({ budgetId, handleClose }) {
   const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } =
     useBudgets();
+
+  const expenses = getBudgetExpenses(budgetId);
 
   const budget =
     UNCATEGORIZED_BUDGET_ID === budgetId
@@ -33,7 +36,28 @@ export default function ViewExpensesModal({ budgetId, handleClose }) {
           </Stack>
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body></Modal.Body>
+      <Modal.Body>
+        <Stack direction='vertical' gap='3'>
+          {expenses.map((expense) => (
+            <Stack direction='horizontal' gap='2' key={expense.id}>
+              <div className='me-auto fs-4'>{expense.description}</div>
+              <div className='fs-5'>
+                {currencyFormatter.format(expense.amount)}
+              </div>
+              <Button
+                size='sm'
+                variant='outline-danger'
+                onClick={() => {
+                  deleteExpense(expense);
+                  handleClose();
+                }}
+              >
+                &times;
+              </Button>
+            </Stack>
+          ))}
+        </Stack>
+      </Modal.Body>
     </Modal>
   );
 }
